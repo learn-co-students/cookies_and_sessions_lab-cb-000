e124144d97d75e58782e3b0a32af8961d38312a4
Laura Felce <crepu83@yahoo.it>
2019-07-04 10:48:32 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,5 @@ class ProductsController < ApplicationController
     render :index
   end
 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
   root 'products#index'# You can have the root of your site routed with "root"
   # root 'welcome#index'
+
   post '/' => 'products#add'
+
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
