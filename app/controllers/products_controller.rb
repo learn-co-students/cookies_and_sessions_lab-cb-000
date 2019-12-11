da937a2020de7166394f02b3cba789b6317a0ed9
Efua Mensah-Brown <emensahb@efbelles-laptop.local>
2019-12-11 12:42:05 -0600

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,12 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = cart
+  end
+
+  def add
+    @product = params[:product]
+    cart << @product
+    redirect_to root_path
+  end
+end
